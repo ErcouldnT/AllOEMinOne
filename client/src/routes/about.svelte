0457c20deca4909d042189e8e45fa1a120882078
ErcouldnT <erkut.tekoglu@gmail.com>
2021-07-17 03:23:48 +0300

    Post a build with form submission :tada:

--- client/src/routes/about.svelte
+++ client/src/routes/about.svelte
@@ -4,12 +4,12 @@
 <h1 class="text-4xl text-center my-8">Nasıl çalışır?</h1>
 <div class="flex flex-col justify-center items-center gap-5">
   <p class="text-center max-w-md">
-    Seçtiğiniz sitelerde anlık arama yaparak
+    1. Seçtiğiniz sitelerde anlık arama yaparak
     "sadece aradığınız kelimeleri içeren" sonuçları
     fiyatına göre listeler.
   </p>
   <p class="text-center max-w-md">
-    Kelimeler arası boşluk bırakarak mümkünse
+    2. Kelimeler arası boşluk bırakarak mümkünse
     noktalama işareti kullanmadan arama yapmanız tavsiye edilir.
   </p>
 </div>
--- client/src/routes/build/index.svelte
+++ client/src/routes/build/index.svelte
@@ -1,10 +1,13 @@
 <script>
   import { build } from '../../stores/build';
   import { fade } from 'svelte/transition';
-  import OEM from '../../lib/Build/OEM.svelte';
+  import OEM from '../../lib/Build/Product.svelte';
 
   let builds = $build;
   let total = 0;
+  let name = "";
+  let owner = "";
+  let slug = "";
   
   $: {
     total = 0;
@@ -18,6 +21,38 @@
     $build = builds;
   }
 
+  let share_button = 'Paylaş';
+  let link = '';
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const system = {
+      name,
+      slug,
+      owner,
+      build: builds
+    };
+    // console.log(sistem);
+    const res = await fetch('/api/build', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(system)
+    });
+    const ok = await res.json();
+    console.log(ok);
+    if (ok.message) {
+      share_button = 'Url değiştir.'
+      slug = '';
+    } else {
+      share_button = 'Paylaşıldı';
+      link = slug;
+      name = "";
+      owner = "";
+      slug = '';
+    }
+  };
 </script>
 
 <svelte:head>
@@ -38,9 +73,35 @@
     </p>
   </div>
 {:else}
-  <div in:fade class="flex justify-center items-center p-6">
+  <div in:fade class="flex flex-col justify-center items-center p-6 gap-3">
     <p class="text-center max-w-md text-xl">
       Toplam: <span class="font-bold">₺{total}</span>
     </p>
+
+    <form class="gap-2 mt-5">
+      <label class="block">
+        <span class="text-gray-700">Sistem adı</span>
+        <input bind:value={name} class="form-input mt-1 block w-full border-2" placeholder="Technopat Temmuz 2021">
+      </label>
+      <label class="block">
+        <span class="text-gray-700">İsminiz</span>
+        <input bind:value={owner} class="form-input mt-1 block w-full border-2" placeholder="Ercode">
+      </label>
+      <label class="block">
+        <span class="text-gray-700">bul.erkuttekoglu.com/build/???</span>
+        <input bind:value={slug} class="form-input mt-1 block w-full border-2" placeholder="sistem1">
+      </label>
+      <div class="flex justify-end">
+        <button on:click={handleClick} class="mt-3 items-center bg-blue-500 hover:bg-blue-700 
+        text-white font-bold py-2 px-4 rounded text-lg">{share_button}</button>
+      </div>
+    </form>
+
   </div>
 {/if}
+
+{#if share_button === 'Paylaşıldı'}
+  <a href={`/build/${link}`} class="flex flex-col justify-center items-center">Sistem {link} linkiyle paylaşılabilir.</a>
+  <a href={`/build/${link}`} class="flex flex-col justify-center items-center">{`https://bul.erkuttekoglu.com/build/${link}`}</a>
+{/if}
+
