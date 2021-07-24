98cae8802062d9c01dfdda9f352e2cf58e4339cc
ErcouldnT <erkut.tekoglu@gmail.com>
2021-07-25 01:16:27 +0300

    Fix: itopya to its own fast api

--- module/itopya.js
+++ module/itopya.js
@@ -18,30 +18,30 @@ async function itopya(search) {
     const page = await browser.newPage();
     // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0');
     await page.goto(url);
-    await page.waitForSelector('div.featured-products', {visible: true});
+    await page.waitForSelector('div.search-products', { visible: true });
     const content = await page.content();
 
     const $ = cheerio.load(content);
-    $('div.featured-products div.product').each((i, e) => {
+    $('div.search-products a').each((i, e) => {
       const $element = $(e);
-      const $price = $element.find('div.product-info div div.new').text().trim();
+      const $price = $element.find('div.text-right').text().trim();
       if ($price) {
-        const title = $element.find('div div.product-title a').text().trim();
+        const title = $element.find('div.col-md-8').text().trim();
         const isTitleValid = Selector(title, search);
 
         if (isTitleValid) {
           const product = {
             title,
-            price: $price.split(' ')[0].slice(0, -3).replace('.', ''),
-            // TODO: if there is more than one "." replace the last one to ","
-            url: $element.find('div div.product-title a').attr('href'),
+            price: $price.replace(".", ""),
+            url: $element.attr('href'),
+            img: $element.find('img').attr('src'),
             source: 'itopya'
           };
           products.push(product);
         }
       }
     });
-
+    // await page.waitForTimeout(9999 * 1000);
     await browser.close();
   } catch (error) {
     console.log(error);
@@ -49,8 +49,8 @@ async function itopya(search) {
   return products.sort((a, b) => a.price - b.price);
 }
 
-Promise.all([itopya("ryzen 5600")]).then(results => {
-  console.log(results);
-});
+// Promise.all([itopya("ryzen 5600")]).then(results => {
+//   console.log(results);
+// });
 
-// module.exports = itopya;
+module.exports = itopya;
